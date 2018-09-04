1e0b88c16c338520f1d210de3b22d11020895bc7
crabloveloli <crabloveloli@gmail.com>
2018-09-04 16:49:26 +0800

    update

--- search/until.py
+++ search/until.py
@@ -280,9 +280,9 @@ class Until:
             return result
     
     # field - readhot/downloads
-    def updateFieldCount(self, es_id, field):
+    def updateFieldCount(self, meta_id, field):
         # url = 'http://127.0.0.1:9200/datastore/doc/8tiNEmUBw7RIvfsV9-Cj/_update'
-        url = '/'.join([settings.ES_URL, settings.ES_INDEX_NAME, settings.ES_INDEX_TYPE, es_id, '_update'])
+        url = '/'.join([settings.ES_URL, settings.ES_INDEX_NAME, settings.ES_INDEX_TYPE, meta_id, '_update'])
         data = {
             "script": {
                 "inline": "ctx._source." + field +" += 1"
@@ -334,20 +334,6 @@ class Until:
                 return hit_list
         # no match
         return []
-    
-    def makeUpFilterList(self, filterList):
-        # "1-传播,1-传播-1-媒体,1-传播-2-媒体测试,2-商业-1-贸易,3-民生,3-民生-1-社保,3-民生-2-社保测试"
-        madeUpList = []
-        fList = str.split(',')
-        for item in fList:
-            arr = item.split('-')
-            
-            if len(arr) == 2:
-                madeUpList.append(item)
-            elif len(arr) == 4:
-                madeUpList.append(item)
-        
-        return filterList
     
     def getESQueryFromChecked(self, filterList, mustQuery):
         if len(filterList) == 0:
